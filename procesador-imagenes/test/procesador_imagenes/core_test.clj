296677a1290377b0ccfce3315fe67dcbd1e5cade
Jean-Luka-Arena <jarena@fi.uba.ar>
2026-06-03 19:02:06 -0300

    esqueleto

---
+++ procesador-imagenes/test/procesador_imagenes/core_test.clj
@@ -0,0 +1,7 @@
+(ns procesador-imagenes.core-test
+  (:require [clojure.test :refer :all]
+            [procesador-imagenes.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
